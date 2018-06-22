90a4ff0e4d140a263377b883b5a3129095c1b311
Gaurav Maheshwari <gmaheshwari3@csc.com>
2018-06-22 16:58:57 +0530

    heroku and work in progress html page implementation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+/*jshint esversion : 6*/
 console.log("starting..");
+const express = require("express");
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+express()
+    .use(express.static(path.join(__dirname, 'app')))
+    .get('/', (req, res) => res.sendFile(path.join(__dirname, "app", "index.html")))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
